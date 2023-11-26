e73bc81164dec31a29d01ce10fc3666a5b595e5f
staroperator <staroperator92@gmail.com>
2023-11-26 15:55:09 -0500

    rename Norm to Normalization

--- Lambda.lean
+++ Lambda.lean
@@ -1,6 +1,12 @@
-import Lambda.Fin
-import Lambda.STLC.Extrinsic.Basic
-import Lambda.STLC.Extrinsic.Typing
-import Lambda.STLC.Extrinsic.Reduce
-import Lambda.STLC.Extrinsic.Norm
+-- import Lambda.Fin
+-- import Lambda.STLC.Extrinsic.Basic
+-- import Lambda.STLC.Extrinsic.Typing
+-- import Lambda.STLC.Extrinsic.Reduce
+-- import Lambda.STLC.Extrinsic.Norm
+import Lambda.Prelude
+import Lambda.STLC.Syntax
+import Lambda.STLC.Operational
+import Lambda.STLC.Confluence
+import Lambda.STLC.Normalization
+import Lambda.STLC.Denotational
 import Lambda.Domain
--- Lambda/Prelude.lean
+++ Lambda/Prelude.lean
@@ -4,6 +4,7 @@ import Mathlib.Tactic.ApplyAt
 import Mathlib.Tactic.SolveByElim
 import Mathlib.Tactic.Substs
 import Mathlib.Tactic.SimpRw
+import Mathlib.Tactic.Congr!
 import Aesop
 
 -- attribute [match_pattern] EmptyCollection.emptyCollection
